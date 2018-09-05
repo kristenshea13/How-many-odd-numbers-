667759b553263a1eeb038e1139d09ba10b9bd03b
Kristen Shea <kstar61@live.com>
2018-09-05 17:26:32 -0500

    Add project files.

---
+++ howmanyoddnumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace howmanyoddnumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
