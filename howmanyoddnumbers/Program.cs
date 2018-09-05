f35d593481589718b809d1918c1f44306bad0a8e
Kristen Shea <kstar61@live.com>
2018-09-05 18:02:44 -0500

    *

--- howmanyoddnumbers/Program.cs
+++ howmanyoddnumbers/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace howmanyoddnumbers
 {
@@ -6,7 +7,36 @@ namespace howmanyoddnumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Count how many odd numbers are in a list.
+
+            //List<string> optionList = new List<string>
+            //{ "AdditionalCardPersonAdressType", /* rest of elements */ };
+
+            List<int> numbers = new List<int>();
+
+            Console.WriteLine("Enter up to 50 numbers to add to list.");
+
+
+            for (int i = 0; i < 50; i++)
+            {
+                Console.Write("Enter number {0}: ", i + 1);
+
+                int userNumber = int.Parse(Console.ReadLine());
+                
+                numbers.Add(userNumber);
+
+                if (string.IsNullOrWhiteSpace(userNumber.ToString())) break;
+
+            }
+
+
+
+
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
